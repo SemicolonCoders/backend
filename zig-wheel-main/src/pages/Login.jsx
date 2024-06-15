80dab0d76dfc35e66fad32fad39473c78840e702
SemicolonCoders <pragatis924@gmail.com>
2024-06-15 14:51:28 +0530

    added vercel

--- zig-wheel-main/src/pages/Login.jsx
+++ zig-wheel-main/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/user/login', {
+      const response = await fetch('https://zig-backend.vercel.app/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
